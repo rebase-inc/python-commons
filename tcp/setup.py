6f1e2fbb0f3304d9d71148e671a334f4e205c169
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2016-11-29 13:16:50 -0800

    Fix reference to requirements.txt

--- tcp/setup.py
+++ tcp/setup.py
@@ -1,3 +1,4 @@
+import os 
 from setuptools import setup
 from setuptools.dist import Distribution
 
@@ -5,7 +6,8 @@ class BinaryDistribution(Distribution):
   def is_pure(self):
     return False
 
-with open('./requirements.txt') as reqs:
+WORKING_DIR = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(WORKING_DIR, 'requirements.txt')) as reqs:
   REQUIREMENTS = [ req for req in reqs ]
 
 setup(name='tcp', version='0.0.1', distclass = BinaryDistribution, install_requires = REQUIREMENTS)
