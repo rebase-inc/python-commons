b38cf72ed838cdc7fa382618bf7e06ec01a28a9c
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2016-12-06 13:14:39 -0800

    Changed build layout

--- build.sh
+++ build.sh
@@ -8,21 +8,15 @@ build_images() {
 RED="\033[0;31m"
 NC="\033[0m"
 
-type=${BASH_ARGV[0]:-build}
+type=${BASH_ARGV[0]:-dev}
 args=${@:1:$#-1}
 
-if [ $type = "devbuild" ]; then
-  echo -e "${RED}Building dev build environment...${NC}"
-  build_images "-f" "composefiles/build.dev.yml" $args
-elif [ $type = "build" ]; then
-  echo -e "${RED}Building build environment...${NC}"
-  build_images "-f" "composefiles/build.yml" $args
-elif [ $type = "prod" ]; then
-  echo -e "${RED}This repository does not include any production products...${NC}"
-  exit 1
-elif [ $type = "dev" ]; then
-  echo -e "${RED}This repository does not include any production products...${NC}"
-  exit 1
+if [ $type = "dev" ]; then
+  echo -e "${RED}Building development environment...${NC}"
+  build_images "-f" "layouts/dev.yml" $args
+elif [ $type = "pro" ]; then
+  echo -e "${RED}Building production environment...${NC}"
+  build_images "-f" "layouts/pro.yml" $args
 else 
   echo -e "${RED}Unknown build type $type${NC}"
   exit 1
--- composefiles/README.md
+++
@@ -1,2 +0,0 @@
-build.yml -> Used to bring up the python commons server. Called "build.yml" because it is part of the build step of the Rebase world
-build.dev.yml -> Build the python commons server in dev mode which, for now, just includes a port mapping so you can play with pypi server
