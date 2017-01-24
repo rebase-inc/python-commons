19c693ee1fec06bced5988f03e6e07f3d58e3445
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2017-01-23 21:51:36 -0800

    Remove stdlib refs

--- libs/codeparser/javascriptparser.py
+++ libs/codeparser/javascriptparser.py
@@ -6,22 +6,9 @@ PARSER_HOST = 'javascript_parser'
 PARSER_PORT = 7777
 IMPACT_HOST = 'javascript_impact'
 IMPACT_PORT = 9999
-STDLIB = set(module.split('.')[0] for module in [
-    'Infinity', 'NaN', 'undefined', 'null', 'eval', 'isFinite', 'isNaN', 'parseFloat',
-    'parseInt', 'decodeURI', 'decodeURIComponent', 'encodeURI', 'encodeURIComponent', 'escape',
-    'unescape', 'Object', 'Function', 'Boolean', 'Symbol', 'Error', 'EvalError', 'InternalError',
-    'RangeError', 'ReferenceError', 'SyntaxError', 'TypeError', 'URIError', 'Number', 'Math',
-    'Date', 'String', 'RegExp', 'Array', 'Int8Array', 'Uint8Array', 'Uint8ClampedArray',
-    'Int16Array', 'Int16Array', 'Uint16Array', 'Int32Array', 'Uint32Array', 'Float32Array',
-    'Float64Array', 'Map', 'Set', 'WeakMap', 'WeakSet', 'SIMD', 'SIMD.Float32x4', 'SIMD.Float64x2',
-    'SIMD.Int8x16', 'SIMD.Int16x8', 'SIMD.Int32x4', 'SIMD.Uint8x16', 'SIMD.Uint16x8', 'SIMD.Uint32x4',
-    'SIMD.Bool8x16', 'SIMD.Bool16x8', 'SIMD.Bool32x4', 'SIMD.Bool64x2', 'ArrayBuffer', 'SharedArrayBuffer',
-    'Atomics', 'DataView', 'JSON', 'Promise', 'Generator', 'GeneratorFunction', 'Reflect', 'Proxy',
-    'Intl', 'Intl.Collator', 'Intl.DateTimeFormat', 'Intl.NumberFormat', 'arguments' ])
 
 class JavascriptParser(LanguageParser):
     language = 'javascript'
-    stdlib = STDLIB
 
     def __init__(self, callback):
         super().__init__(callback)
@@ -32,7 +19,7 @@ class JavascriptParser(LanguageParser):
         return super().get_context(repo_name, commit, path)
 
     def check_relevance(self, module):
-        return module.split('.')[0] in self.stdlib or super().check_relevance(module)
+        return super().check_relevance(module)
 
     @property
     def relevance_checker(self):
